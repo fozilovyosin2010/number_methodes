0ce166bb2dd231a824b415349aeae706e3d73d5e
yosin <umnovanika1@gmail.com>
2024-03-17 21:57:35 +0500

    MATH ROUND

--- name.js
+++ name.js
@@ -5,3 +5,9 @@
 // let num = 34.5;
 // let print = Math.floor(num);
 // console.log(print); SHOWING A FLOAT NUM
+
+//Math round
+
+// let num = 34.4;
+// let print = Math.round(num);
+// console.log(print); 34.4 ; 34.5=35;
